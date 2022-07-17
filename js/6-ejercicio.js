893f35336c6ba9c4378f3937f2d40604a281c3ed
MajoA <majoaguero20@hotmail.comgit config --global user.name MajoAgit commit -m 'primer commit del proyecto de la panaderia''git config --global user.email majoaguero20@hotmail.com>
2022-07-17 19:34:16 -0300

    ejercicio 6 realizado

---
+++ js/6-ejercicio.js
@@ -0,0 +1,18 @@
+// Realiza un script que escriba una pirámide del 1 al 30 de la siguiente forma :
+
+// 1
+// 22
+// 333
+// 4444
+// 55555
+// 666666
+let x = 1
+let i = 0
+for (x = 0; x <= 30 ; x++ ){
+    for ( i = 0; i < x ; i++ ){
+        document.write (x)
+        
+    }
+    document.write ('<br>')
+
+}
