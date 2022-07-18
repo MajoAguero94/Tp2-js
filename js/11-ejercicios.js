1804a948a64a611571cc26441223dd4c9eefbe94
MajoA <majoaguero20@hotmail.comgit config --global user.name MajoAgit commit -m 'primer commit del proyecto de la panaderia''git config --global user.email majoaguero20@hotmail.com>
2022-07-18 13:43:28 -0300

    ejercicio 11 actualizado

--- js/11-ejercicios.js
+++ js/11-ejercicios.js
@@ -1,8 +1,28 @@
 //Realiza un script que pida por teclado 3 edades y 3 nombres e indique el nombre del mayor. 
 
-let numero1 = parseInt(prompt('Ingrese nombre y edad del usuario 1'))
-let numero2 = parseInt(prompt('Ingrese nombre y edad del usuario 2'))
-let numero3 = parseInt(prompt('Ingrese nombre y edad del usuario 3'))
+       
+   
+    let nombre1 = prompt("Nombre 1:");
+    let edad1 = Number(prompt("Edad 1:"));
+
+    let nombre2 = prompt("Nombre 2:");
+    let edad2 = Number(prompt("Edad 2:"));
+
+    let nombre3 = prompt("Nombre 3:");
+    let edad3 = Number(prompt("Edad 3:"));
+
+   
+    let maximo = Math.max(edad1, edad2, edad3);
+
+    if (maximo == edad1) {
+        document.write("El mayor es: " + nombre1);
+    }
+    if (maximo == edad2) {
+        document.write("El mayor es: " + nombre2);
+    }
+    if (maximo == edad3) {
+        document.write("El mayor es: " + nombre3);
+    }
 
 
 
