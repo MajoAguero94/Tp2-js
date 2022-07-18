a3bea35b20d6c080f5c1247eee27ed8ba7c1b90a
MajoA <majoaguero20@hotmail.comgit config --global user.name MajoAgit commit -m 'primer commit del proyecto de la panaderia''git config --global user.email majoaguero20@hotmail.com>
2022-07-18 13:20:21 -0300

    ejercicio 8 actualizado

--- js/8-ejercicio.js
+++ js/8-ejercicio.js
@@ -13,9 +13,9 @@ let x = 1
 let y = 0
 
 if (numero > 1 && numero <= 50) {
-    for (x= 1; x<= numero; x++) {
-        for(y=0; y<x; y++ ) {
-            document.write (x)
+    for (x= 0; x<= numero; x++) {
+        for(y=1; y<x; y++ ) {
+            document.write (y)
         }
         document.write ('<br>')
     }
